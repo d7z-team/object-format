df2b1ed08727baa0c85ce8acd97b1962eb7fa12d
dragon <33776693+ExplodingDragon@users.noreply.github.com>
2022-02-12 22:35:14 +0800

    feat(extra-time): 新增 Date 格式化

--- format-extra-time/src/main/java/module-info.java
+++ format-extra-time/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 import org.d7z.objects.format.api.IFormat;
+import org.d7z.objects.format.ext.time.DateFormat;
 import org.d7z.objects.format.ext.time.DateTimeFormat;
 
 module org.d7z.objects.format.ext.time {
@@ -7,5 +8,5 @@ module org.d7z.objects.format.ext.time {
     requires transitive org.d7z.objects.format.core;
     opens org.d7z.objects.format.ext.time;
     exports org.d7z.objects.format.ext.time;
-    provides IFormat with DateTimeFormat;
+    provides IFormat with DateTimeFormat, DateFormat;
 }
---
+++ format-extra-time/src/main/kotlin/org/d7z/objects/format/ext/time/DateFormat.kt
@@ -0,0 +1,26 @@
+package org.d7z.objects.format.ext.time
+
+import org.d7z.objects.format.CovertException
+import org.d7z.objects.format.api.IFormat
+import org.d7z.objects.format.api.IFormatContext
+import java.util.Date
+import kotlin.reflect.KClass
+
+class DateFormat : IFormat {
+    override val types = setOf(Date::class)
+
+    override fun <T : Any> format(data: T, type: KClass<out T>, context: IFormatContext): String {
+        if (type == Date::class && data is Date) {
+            return data.time.toString()
+        }
+        throw CovertException("无法执行数据转换.")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : Any> reduce(data: String, type: KClass<T>, context: IFormatContext): T {
+        if (type == Date::class) {
+            return Date(data.toLong()) as T
+        }
+        throw CovertException("无法执行数据转换.")
+    }
+}
